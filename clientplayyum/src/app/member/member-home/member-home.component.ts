7e398d432b966660bac01fa2db83d8b127df554b
DTully2 <tully.dev@gmail.com>
2022-10-26 22:03:46 -0400

    signincomingalong

--- clientplayyum/src/app/member/member-home/member-home.component.ts
+++ clientplayyum/src/app/member/member-home/member-home.component.ts
@@ -9,9 +9,19 @@ import { MemberService } from '../member.service';
 export class MemberHomeComponent implements OnInit {
   members: Array<Member>;
   msg: string;
+  hideEditForm: boolean;
+  member: Member;
+
   constructor(public memberService: MemberService) {
+    this.member={
+      id: 0,
+      password: '',
+      email: '',
+    
+    };
     this.members = [];
     this.msg = '';
+    this.hideEditForm = true;
   } // constructor
   ngOnInit(): void {
     this.msg = 'loading members from server...';
@@ -26,6 +36,41 @@ export class MemberHomeComponent implements OnInit {
       complete: () => {},
     }); // subscribe
   } // ngOnInit
+   /**
+   * save - determine whether we're doing and add or an update
+   */
+    save(vendor: Member): void {
+      vendor.id ? this.update(vendor) : this.add(vendor);
+    } // save
+  
+      add(vendor: Member): void {
+    vendor.id = 0;
+    this.memberService.add(vendor).subscribe({
+      // Create observer object
+      next: (emp: Member) => {
+        this.msg = `Vendor ${emp.id} added!`;
+      },
+      error: (err: Error) => (this.msg = `Vendor not added! - ${err.message}`),
+      complete: () => (this.hideEditForm = !this.hideEditForm),
+    });
+  } // add
+  update(vendor: Member): void {
+    this.memberService.update(vendor).subscribe({
+      // Create observer object
+      next: (emp: Member) => (this.msg = `Members ${emp.email} updated!`),
+      error: (err: Error) => (this.msg = `Update failed! - ${err.message}`),
+      complete: () => (this.hideEditForm = !this.hideEditForm),
+    });
+  } // update
+  newVendor(): void {
+    this.msg = 'New Vendor';
+    this.member = {
+      id: 0,
+      password: '',
+      email: '',
+    };
+    this.hideEditForm = !this.hideEditForm;
+  } // newVendor
 } // MemberHomeComponent
 
 
--- clientplayyum/src/app/member/member.service.ts
+++ clientplayyum/src/app/member/member.service.ts
@@ -4,61 +4,66 @@ import { BASEURL } from '../constants';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { Member } from './member';
+import { GenericHttpService } from '@app/generic-http.service';
 @Injectable({
   providedIn: 'root',
 })
-export class MemberService {
-  resourceURL: string;
-  status: string;
-  constructor(public http: HttpClient) {
-    this.resourceURL = `${BASEURL}members`;
-    this.status = '';
-  } // constructor
-  /**
-   * Retrieves the employeee JSON, then returns the array to a subscriber
-   * we're temporarily using an any type (typically a bad idea) because the Spring Boot
-   * repository returns all the data in an "embedded" property
-   */
-  get(): Observable<any> {
-    return this.http
-      .get(this.resourceURL)
-      .pipe(retry(1), catchError(this.handleError));
-  } // get
-  /**
-    * Update an employee on the server using http put, server returns
-    * updated employee, then return it as Observable to caller
-    */
-  update(member: Member): Observable<Member> {
-    return this.http
-      .put<Member>(`${this.resourceURL}`, member)
-      .pipe(retry(1), catchError(this.handleError));
-  } // update
-  // Error handling
-  handleError(error: any) {
-    let errorMessage = '';
-    error.error instanceof ErrorEvent
-      ? // Get client-side error
-        (errorMessage = error.error.message)
-      : // Get server-side error
-        (errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`);
-    window.alert(errorMessage); // probably should console.log when going into production
-    return throwError(() => errorMessage);
-  }
-  //   /**
-  //    * add an employee on the server via POST, return Observable
-  //    */
-  add(member: Member): Observable<Member> {
-    member.id = 0;
-    return this.http
-      .post<Member>(this.resourceURL, member)
-      .pipe(retry(1), catchError(this.handleError));
-  } // add
-  //   /**
-  //  * delete an employee on the server, return Observable
-  //  */
-  delete(id: number): Observable<number> {
-    return this.http
-      .delete<number>(`${this.resourceURL}/${id}`)
-      .pipe(retry(1), catchError(this.handleError));
-  } // delete
-} // Member service
+export class MemberService extends GenericHttpService<Member> {
+  constructor(httpClient: HttpClient) {
+    super(httpClient, 'members');
+    } // constructor
+}
+//   resourceURL: string;
+//   status: string;
+//   constructor(public http: HttpClient) {
+//     this.resourceURL = `${BASEURL}members`;
+//     this.status = '';
+//   } // constructor
+//   /**
+//    * Retrieves the employeee JSON, then returns the array to a subscriber
+//    * we're temporarily using an any type (typically a bad idea) because the Spring Boot
+//    * repository returns all the data in an "embedded" property
+//    */
+//   get(): Observable<any> {
+//     return this.http
+//       .get(this.resourceURL)
+//       .pipe(retry(1), catchError(this.handleError));
+//   } // get
+//   /**
+//     * Update an employee on the server using http put, server returns
+//     * updated employee, then return it as Observable to caller
+//     */
+//   update(member: Member): Observable<Member> {
+//     return this.http
+//       .put<Member>(`${this.resourceURL}`, member)
+//       .pipe(retry(1), catchError(this.handleError));
+//   } // update
+//   // Error handling
+//   handleError(error: any) {
+//     let errorMessage = '';
+//     error.error instanceof ErrorEvent
+//       ? // Get client-side error
+//         (errorMessage = error.error.message)
+//       : // Get server-side error
+//         (errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`);
+//     window.alert(errorMessage); // probably should console.log when going into production
+//     return throwError(() => errorMessage);
+//   }
+//   //   /**
+//   //    * add an employee on the server via POST, return Observable
+//   //    */
+//   add(member: Member): Observable<Member> {
+//     member.id = 0;
+//     return this.http
+//       .post<Member>(this.resourceURL, member)
+//       .pipe(retry(1), catchError(this.handleError));
+//   } // add
+//   //   /**
+//   //  * delete an employee on the server, return Observable
+//   //  */
+//   delete(id: number): Observable<number> {
+//     return this.http
+//       .delete<number>(`${this.resourceURL}/${id}`)
+//       .pipe(retry(1), catchError(this.handleError));
+//   } // delete
+// } // Member service
