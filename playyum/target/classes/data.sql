b46d508d2a48a403d2b27a7925530767e7d6cb24
Devon Tully <72405785+DTully2@users.noreply.github.com>
2022-12-22 13:52:52 -0500

    d

---
+++ playyum/target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Member (Username,Email,password,points,avatar,dice)
+VALUES ('Bigshot','bs@abc.com','1234',10000,'','white');
+
+INSERT INTO Item (name,img,price,type) VALUES ('white','white_1.png',0,'dice');
+INSERT INTO Item (name,img,price,type) VALUES ('avatar1','avatar1.png',10,'avatar');
+
+INSERT INTO Item (name,img,price,type) VALUES ('avatar2','avatar2.png',200,'avatar');
+INSERT INTO Item (name,img,price,type) VALUES ('red','red_1.png',500,'dice');
